17191c33c42ce419753e65ac5775ac0e008d1fd2
André Arko <andre@arko.net>
2025-03-02 20:27:58 -0800

    test that comment methods return 422s

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -7,44 +7,78 @@ describe "#comments" do
 
   it "#newComment" do
     VCR.use_cassette("newComment") do
-      COMMENT_ID = CLIENT.newComment({:post_id => post_id, :comment => comment})
-      COMMENT_ID.class.should eq(Integer)
+      Proc.new do
+        COMMENT_ID = CLIENT.newComment({:post_id => post_id, :comment => comment})
+        # COMMENT_ID.class.should eq(Integer)
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 
   it "#editComment" do
     VCR.use_cassette("editComment") do
-      CLIENT.editComment({:comment_id => COMMENT_ID, :comment => comment}).class.should eq(TrueClass)
+      Proc.new do
+        CLIENT.editComment({:comment_id => 1, :comment => comment}).class.should eq(TrueClass)
+
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 
   it "#getCommentCount" do
     VCR.use_cassette("getCommentCount") do
-      CLIENT.getCommentCount({:post_id => post_id}).should include("approved")
+      Proc.new do
+        CLIENT.getCommentCount({:post_id => post_id}).should include("approved")
+
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 
   it "#getComment" do
     VCR.use_cassette("getComment") do
-      CLIENT.getComment({:comment_id => COMMENT_ID}).should include("content" => "This is a test thing here.")
+      Proc.new do
+        CLIENT.getComment({:comment_id => 1}).should include("content" => "This is a test thing here.")
+
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 
   it "#getComments" do
     VCR.use_cassette("getComments") do
-      CLIENT.getComments[0].should include("post_id" => post_id.to_s)
+      Proc.new do
+        CLIENT.getComments[0].should include("post_id" => post_id.to_s)
+
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 
   it "#deleteComment" do
     VCR.use_cassette("deleteComment") do
-      CLIENT.deleteComment({:comment_id => COMMENT_ID}).class.should eq(TrueClass)
+      Proc.new do
+        CLIENT.deleteComment({:comment_id => 1}).class.should eq(TrueClass)
+
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 
   it "#getCommentStatusList" do
     VCR.use_cassette("getCommentStatusList") do
-      CLIENT.getCommentStatusList.should include("hold" => "Unapproved")
+      Proc.new do
+        CLIENT.getCommentStatusList.should include("hold" => "Unapproved")
+
+      end.should raise_error(RuntimeError)
+      CLIENT.instance_eval { @connection }.http_last_response.code.should eq "422"
+
     end
   end
 

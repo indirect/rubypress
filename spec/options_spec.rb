cc2bdd0dd54da45b60d041ddd2d3454dd3e542d0
André Arko <andre@arko.net>
2025-03-09 17:17:18 -0700

    align the expected hashes

--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -6,13 +6,17 @@ describe "#options" do
 
   it "#getOptions" do
     VCR.use_cassette("getOptions") do
-      CLIENT.getOptions.should include("baseURL" => {"desc"=>"Baseurl", "readonly"=>false, "value"=>"https://andre.arko.net/"})
+      CLIENT.getOptions.should include("baseURL" => {
+        "desc"=>"Baseurl", "readonly"=>false, "value"=>"https://andre.arko.net/"
+      })
     end
   end
 
   it "#setOptions" do
     VCR.use_cassette("setOptions") do
-      CLIENT.setOptions({:options => options}).should include("blog_tagline" => {"desc"=>"Blog Tagline", "readonly"=>false, "value"=>"This is a great tagline"})
+      CLIENT.setOptions({:options => options}).should include("blog_tagline" => {
+        "desc"=>"Blog Tagline", "readonly"=>false, "value"=>"This is a great tagline"
+      })
     end
   end
 
